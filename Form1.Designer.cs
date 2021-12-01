dc736da136b5cbc4b9b5ca2b1703424f7ab694dd
owowhat <uwuwotisdis@gmail.com>
2021-12-01 11:27:36 -0500

    add everything
    yep

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace prntsc_viewer_because_cloudflare_is_a_bitch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idInput = new System.Windows.Forms.TextBox();
+            this.linkInput = new System.Windows.Forms.TextBox();
+            this.urlLabel = new System.Windows.Forms.LinkLabel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // idInput
+            // 
+            this.idInput.Location = new System.Drawing.Point(12, 12);
+            this.idInput.Name = "idInput";
+            this.idInput.Size = new System.Drawing.Size(70, 20);
+            this.idInput.TabIndex = 0;
+            this.idInput.Text = "ID Input";
+            this.idInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.idInput_KeyPress);
+            // 
+            // linkInput
+            // 
+            this.linkInput.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkInput.Location = new System.Drawing.Point(182, 12);
+            this.linkInput.Name = "linkInput";
+            this.linkInput.Size = new System.Drawing.Size(70, 20);
+            this.linkInput.TabIndex = 1;
+            this.linkInput.Text = "Link Input";
+            this.linkInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.linkInput_KeyPress);
+            // 
+            // urlLabel
+            // 
+            this.urlLabel.AutoSize = true;
+            this.urlLabel.Location = new System.Drawing.Point(9, 35);
+            this.urlLabel.Name = "urlLabel";
+            this.urlLabel.Size = new System.Drawing.Size(47, 13);
+            this.urlLabel.TabIndex = 4;
+            this.urlLabel.TabStop = true;
+            this.urlLabel.Text = "link here";
+            this.urlLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 51);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(126, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Auto-open in browser";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 74);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.urlLabel);
+            this.Controls.Add(this.linkInput);
+            this.Controls.Add(this.idInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox idInput;
+        private System.Windows.Forms.TextBox linkInput;
+        private System.Windows.Forms.LinkLabel urlLabel;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+using System.Net.Http;
+
+namespace prntsc_viewer_because_cloudflare_is_a_bitch
+{
+    public partial class Form1 : Form
+    {
+        WebClient client = new WebClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string DownloadImage(string id)
+        {
+            client.Headers.Add(HttpRequestHeader.UserAgent, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36");
+
+            string prntscHtml = client.DownloadString("https://prnt.sc/" + id);
+            Regex prntscRegex = new Regex(@"https://image\.prntscr\.com/image/([a-zA-Z0-9_-]+)\.png");
+            Regex imgurRegex = new Regex(@"https://i\.imgur\.com/([a-zA-Z0-9_-]+)\.png");
+            bool prntscMatches = prntscRegex.IsMatch(prntscHtml);
+            string imageUrl;
+            if (prntscMatches) { imageUrl = prntscRegex.Match(prntscHtml).ToString(); }
+            else { imageUrl = imgurRegex.Match(prntscHtml).ToString(); }
+
+
+            client.Headers.Add("Access-Control-Allow-Origin", "*");
+            client.Headers.Add("Access-Control-Allow-Methods", "GET, OPTIONS");
+            client.Headers.Add("Access-Control-Allow-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type");
+
+            /*try
+            {
+                imageBytes = client.DownloadData(imageUrl.Value);
+            }
+            catch (WebException ex)
+            {
+                WebException Exception = ex;
+                HttpWebResponse Response = (HttpWebResponse)Exception.Response;
+                StreamReader WebStream = new StreamReader(Response.GetResponseStream(), System.Text.Encoding.GetEncoding("utf-8"));
+                imageBytes = Encoding.UTF8.GetBytes(WebStream.ReadToEnd());
+                Debug.WriteLine(imageBytes);
+                Debug.WriteLine(WebStream.ReadToEnd());
+            }*/
+            //HttpWebRequest imageRequest = (HttpWebRequest)WebRequest.Create(imageUrl.Value);
+            //imageRequest.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36";
+            //imageRequest.UseDefaultCredentials = true;
+            //HttpWebResponse imageResponse = (HttpWebResponse)imageRequest.GetResponse();
+            //Stream imageStream = imageResponse.GetResponseStream();
+            switch (imageUrl)
+            {
+                case "":
+                    return "Link returned with nothing!";
+                default:
+                    return imageUrl;
+            }
+        }
+
+        private void idInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                urlLabel.Text = DownloadImage(idInput.Text);
+                ActiveForm.Width = urlLabel.Width + 33;
+                ActiveForm.Height = 113;
+                if (checkBox1.Checked) { Process.Start(urlLabel.Text); }
+            }
+        }
+
+        private void linkInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                urlLabel.Text = DownloadImage(linkInput.Text);
+                ActiveForm.Width = urlLabel.Width + 33;
+                ActiveForm.Height = 113;
+                Process.Start(urlLabel.Text);
+                if (checkBox1.Checked) { Process.Start(urlLabel.Text); }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
